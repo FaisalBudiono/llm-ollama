de839e3dd9a71f935deb6c268b781fdb7c435e67
Faisal Budiono <ucul.project@gmail.com>
2026-07-28 12:27:53 +0700

    WIP

---
+++ down.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+
+# docker stop ollama
+# docker rm ollama
+docker compose down --remove-orphans
---
+++ exec.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+# docker exec -ti ollama bash
+docker compose exec ollama bash
